25bbf9dd07d91fc74ab227db0901d2cac7ffbaa9
Jeremy Cade <me@jeremycade.com>
2018-04-01 12:43:56 +1000

    Migrate Windsor Extension

---
+++ src/extensions/Nimbus.Windsor/Configuration/WindsorBusBuilderConfigurationExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Castle.Windsor;
+using Nimbus.DependencyResolution;
+
+// ReSharper disable CheckNamespace
+
+namespace Nimbus.Configuration
+// ReSharper restore CheckNamespace
+{
+    public static class WindsorBusBuilderConfigurationExtensions
+    {
+        public static BusBuilderConfiguration WithWindsorDefaults(this BusBuilderConfiguration configuration, IWindsorContainer container)
+        {
+            return configuration
+                .WithDependencyResolver(container.Resolve<IDependencyResolver>())
+                .WithTypesFrom(container.Resolve<ITypeProvider>());
+        }
+    }
+}
